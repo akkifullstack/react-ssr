d04184006bb3bc2f88f3813a1e47814564b0c697
akkifullstack <saymefit@gmail.com>
2018-07-22 23:20:50 +0530

    adding files

---
+++ src/client/app.js
@@ -0,0 +1,16 @@
+// startup point for the client side application
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './Routes';
+
+
+const App = (
+    <BrowserRouter>
+        <Routes/>
+    </BrowserRouter>
+)
+
+
+ReactDOM.hydrate(App,document.getElementById('root'));
---
+++ src/client/components/home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+
+
+
+const Home = () => {
+    return (
+    <div>
+        <div>I am home of FOOD BLOG project</div>
+        <button onClick={() => console.log("Hi there!!!")}>Press Me!</button>
+    </div>
+)
+}
+
+
+export default Home;
